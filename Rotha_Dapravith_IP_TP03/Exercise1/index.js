76760b6745dc630f6ab34133fec9cd49410bb80a
ROTHA DAPRAVITH <90898700+Dapravith@users.noreply.github.com>
2023-01-20 22:27:44 +0700

    Update index.js

--- Rotha_Dapravith_IP_TP03/Exercise1/index.js
+++ Rotha_Dapravith_IP_TP03/Exercise1/index.js
@@ -36,7 +36,7 @@ function renderList () {
 }
 
 function add (idValue, titleValue, assigneeValue, deadlineValue) {
-    var todo = {id:idVaue, title:titleValue, assignee:assigneeValue, deadline:deadlineValue}
+    var todo = {id:idValue, title:titleValue, assignee:assigneeValue, deadline:deadlineValue}
     todoList.push(todo)
     localStorage.setItem(idValue+"", JSON.stringify(todo))
     title.value = "";
